665c9c29a538cc2d526cf91e9994a946a795685a
Robert Mosolgo <rdmosolgo@gmail.com>
2016-10-10 19:08:19 -0400

    feat(Mask) hide input objects & arguments

--- lib/graphql/static_validation/rules/variables_are_input_types.rb
+++ lib/graphql/static_validation/rules/variables_are_input_types.rb
@@ -13,9 +13,9 @@ module GraphQL
 
       def validate_is_input_type(node, context)
         type_name = get_type_name(node.type)
-        type = context.schema.types[type_name]
-        if !type.kind.input?
-          context.errors << message("#{type.name} isn't a valid input type (on $#{node.name})", node, context: context)
+        type = context.warden.get_type(type_name)
+        if type.nil? || !type.kind.input?
+          context.errors << message("#{type_name} isn't a valid input type (on $#{node.name})", node, context: context)
         end
       end
 
